8a33a7add1b1f178a9868c0bc4e3ab54c536bdf5
hritik12345444 <hritik12345444@gmail.com>
2025-06-10 10:18:13 +0530

    check a and b which is greater

---
+++ questions/checkGreater.py
@@ -0,0 +1,13 @@
+#  input 2 int numbers, a and b.
+# Print True if a is greater than or equal to b. If not print False
+
+
+num1 = int (input("enter n1 : "))
+num2 = int (input("enter num2 : "))
+
+
+# Conditional statement thoda sa different syntax hota hai c and cpp se
+if(num1 >= num2 ):
+    print("True")
+else:
+    print("False")
