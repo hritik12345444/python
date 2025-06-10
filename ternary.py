084f8c3647bf0dce6608188b6ff4f6b2d625601d
hritik12345444 <hritik12345444@gmail.com>
2025-06-10 19:56:59 +0530

    ternaty operator

---
+++ ternary.py
@@ -0,0 +1,11 @@
+# ternary operator
+# check no is even or odd
+
+num = int(input("num : "))
+res = ("even") if num%2==0 else ("odd")
+print(res)
+
+
+age = int (input("age : "))
+result = "Adult" if age >= 18 else "Minor"
+print(result)
