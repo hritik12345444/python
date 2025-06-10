759c57f7795ff3e574ccf0a516fb7b759d010058
hritik12345444 <hritik12345444@gmail.com>
2025-06-10 20:13:47 +0530

    type casting and type conversion

---
+++ questions/typecasting_typeconverrsion.py
@@ -0,0 +1,12 @@
+# type casting and type conversion
+
+num1 = 11  # int
+num2 = 11.0 # float
+sum = num1+num2 # automatically convert into float. this is called type conversion
+print("this is in float value , this is type conversion" , sum)
+
+# if we want to num1 is cange into float than here use type casting
+num3 = float(num1)  # manually we convert num1 to float value so this is type casting
+num4 = int(num2)
+print(num3)  
+print(num4)
