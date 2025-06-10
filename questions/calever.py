e7523f7820e2396507ef6364c73bc223cb3af5ee
hritik12345444 <hritik12345444@gmail.com>
2025-06-10 20:07:13 +0530

    calever if statement

---
+++ questions/calever.py
@@ -0,0 +1,6 @@
+# calever if statement
+
+num = int(input("num : "))
+res = ("no","yes")[num%2==0]  # if condition is true than right statement execute else left statement execute this is calever if statement.  In calever if statement condition is write in a square brackets
+
+print(res)
