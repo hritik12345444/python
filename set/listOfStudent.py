9642a4c8468d16e5be8053bc3344eadf93ee33a9
hritik12345444 <hritik12345444@gmail.com>
2025-06-13 22:45:49 +0530

    list of student

---
+++ set/listOfStudent.py
@@ -0,0 +1,7 @@
+# You are given a list of subjects for students. Assume one classroom is required for 1 subject. How many classrooms are needed by all students.
+# "python","java","C++","python","javascript","java","python","java","C++",C
+
+my_set = {"python","java","C++","python","javascript","java","python","java","C++","C"}
+
+no_Of_Classroom = len(my_set)
+print(no_Of_Classroom)
