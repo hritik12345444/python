869501c95ba5e8ec2d26c7cfc3d871e5615d409f
hritik12345444 <hritik12345444@gmail.com>
2025-06-20 21:50:06 +0530

    line number find

--- FILES/whichLine.py
+++ FILES/whichLine.py
@@ -2,3 +2,18 @@
 # Print -1 if word not found
 
 
+def check_for_line():
+    word = "python"
+    with open ("practice.txt",'r') as f :
+        count = 1
+        data = True  # data false tbb hoga jabb usme empty string na aa jaye
+        while(data) :
+            data = f.readline()
+            if(word in data):  # same as data.find(word) != -1
+                print(count)
+                break  # jaisse hi mill jaye uske aage code run na hona chahiye
+            else :
+                count+=1
+
+
+check_for_line()
