1340c4012a57306d3b8fca062c8bffe979907b8c
hritik12345444 <hritik12345444@gmail.com>
2025-06-23 09:05:14 +0530

    update debit, credit and balencCheck  in code

--- Objects&Class/account.py
+++ Objects&Class/account.py
@@ -3,22 +3,26 @@
 
 
 class Account:
-    def __init__(self,acc_no, bal):
+    def __init__(self,acc_no, balance):
         self.Acc = acc_no
-        self.balance = bal
+        self.balance = balance
 
     def debit(self,amount):
-        return self.balance-amount
+        print("your acc is debited by Rs :-",amount)
+        self.balance = self.balance - amount
+        print("your current balence is Rs :-",self.balance)
     
     def credit(self,amount):
-        return self.balance+amount
+        print("Your acc is credited by Rs :-",amount)
+        self.balance = self.balance + amount
+        print("your current balence is Rs :-",self.balance)
     
     def balanceCheck(self):
-        print("Your Acc:- ", self.balance)
+        print("Your total-balence is Rs :- ", self.balance)
 
 
 
-acc = Account(12345,5555)
-print(acc.balance)
-print(acc.credit(1000))
-print(acc.debit(3000))
+a = Account("12345",4000)
+a.credit(1000)
+# a.debit(2000)
+# a.balanceCheck()
