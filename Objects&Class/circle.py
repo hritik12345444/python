42cef8741a4dac5b2126376c877efa10feb65393
hritik12345444 <hritik12345444@gmail.com>
2025-06-25 13:50:58 +0530

    area of circle

---
+++ Objects&Class/circle.py
@@ -0,0 +1,29 @@
+# Define a Circle class to create a circle with radius r using the constructor.
+# Define an Area() method of the class which calculates the area of teh circle.
+# Define a Perimeter() method of the class which allows you to calculate the perimeter of the circle.
+
+
+import math # for using math function
+class Circle:
+    def __init__ (self,rad):
+        self.rad = rad
+
+    def Area(self):
+        ara=  math.pi * self.rad ** 2 # (22/7 * r * r ) we can able to do this because we import math
+        print(f"Area = {ara} cm^2")
+        # print(22/7 * self.rad * self.rad)
+    
+    def Perimeter(self):
+        pere = 2 * 22/7 * self.rad
+        
+        print(f"Perimeter = {pere} cm")
+    
+
+c1 = Circle(5)
+c1.Area()
+c1.Perimeter()
+
+c2 = Circle(10)
+c2.Area()
+c2.Perimeter()
+# something 
